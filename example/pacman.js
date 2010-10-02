b31c05e9f28c938805f78dcd987a84776d864316
siong1987 <siong1987@gmail.com>
2010-10-02 13:04:36 -0500

    yo

--- example/pacman.js
+++ example/pacman.js
@@ -64,7 +64,8 @@ function initializeSocket() {
   socket.connect();
   socket.on('message', function(obj){
     var json_data = JSON.parse(obj);
-    if (json_data.coor1 && json_data.coor2) {
+    console.log(json_data);
+    if (json_data.type == 0) {
       var myLatlng = new google.maps.LatLng((json_data.coor1.latitude + json_data.coor2.latitude)/2, (json_data.coor1.longitude + json_data.coor2.longitude)/2);
       map.setCenter(myLatlng);
     }
