e49d64d1af031b02dc3c9deac28228a5b47cfc87
siong1987 <siong1987@gmail.com>
2010-10-02 09:12:34 -0500

    yo

--- example/pacman.js
+++ example/pacman.js
@@ -23,11 +23,8 @@ function initializeSocket() {
   var socket = new io.Socket(null, {port: 8080});
   socket.connect();
   socket.on('message', function(obj){
-    var index = (JSON.parse(obj).y)*5 + (JSON.parse(obj).x + 1);
-    for (i=1;i<=25;i++) {
-      $("#maze"+i).css("background-image", "url('http://cdn.siong1987.com/pacman/empty.png')");
-    }
-    $("#maze"+index).css("background-image", "url('http://cdn.siong1987.com/pacman/maze.png')");
+    var json_data = JSON.parse(obj);
+    console.log(json_data);
   });
 }
 
