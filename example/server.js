5e5b34a82eb344e73203fb79f0c0afb5a422eda7
siong1987 <siong1987@gmail.com>
2010-10-02 02:16:13 -0500

    working node js.

--- example/server.js
+++ example/server.js
@@ -15,10 +15,19 @@ server = http.createServer(function(req, res){
 			break;
 			
 		case '/json.js':
+		case '/socket.js':
+			fs.readFile(__dirname + path, function(err, data){
+				if (err) return send404(res);
+				res.writeHead(200, {'Content-Type': 'text/javascript'})
+				res.write(data, 'utf8');
+				res.end();
+			});
+			break;
+			
 		case '/chat.html':
 			fs.readFile(__dirname + path, function(err, data){
 				if (err) return send404(res);
-				res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+				res.writeHead(200, {'Content-Type': 'text/html'})
 				res.write(data, 'utf8');
 				res.end();
 			});
