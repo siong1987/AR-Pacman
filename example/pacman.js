1129008bad826da949df17e68328c7c641e26e6a
siong1987 <siong1987@gmail.com>
2010-10-02 07:03:52 -0500

    yo

--- example/pacman.js
+++ example/pacman.js
@@ -23,9 +23,8 @@ function initializeSocket() {
   var socket = new io.Socket(null, {port: 8080});
   socket.connect();
   socket.on('message', function(obj){
-    console.log(obj);
-    var index = (obj.y-1)*5 + (obj.x);
-    console.log(index);
+    console.log(obj.y);
+    var index = (JSON.parse(obj).y-1)*5 + (JSON.parse(obj).x);
     for (i=1;i<=25;i++) {
       $("#maze"+index).css("background-image", "url('')");
     }
